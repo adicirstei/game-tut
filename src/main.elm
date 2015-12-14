fc621a4961e2a95422eb384ae77baa69541a5d1e
Adrian <adicirstei@gmail.com>
2015-12-14 17:34:04 +0200

    some types

--- src/main.elm
+++ src/main.elm
@@ -11,8 +11,29 @@ relativeMouse (ox, oy) (x, y) = (x - ox, y - oy)
 center : (Int, Int) -> (Int, Int)
 center (w, h) = (w //2, h // 2)
 
+type Vec = (Float, Float)
+type Pill = {pos : Vec, vel : Vec, rad : Float, col : Color }
+
+
+defaultPill =
+  { pos = (0, 100)
+  , vel = (0, -1)
+  , rad = 15
+  , col = lightRed
+  }
+
+stepPill : Pill -> Pill
+stepPill p =
+  { p | pos = vecAdd p.pos p.vel }
+
+
 render (w, h) =
-  let forms = [circle 15 |> filled lightBlue]
+  let
+    formPill {rad, col, pos} =
+      circle rad
+      |> filled col
+      |> move pos
+    forms = [ formPill defaultPill]
   in
     color lightGray <| container w h middle
       <| color white
