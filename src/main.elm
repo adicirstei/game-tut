93c73fc739308c9d086597b8eb8038cba99a15fd
Adrian <adicirstei@gmail.com>
2015-12-15 16:19:24 +0200

    done video 2

--- src/main.elm
+++ src/main.elm
@@ -1,43 +1,54 @@
 import Mouse
 import Window
 import Signal
+import Time
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 
+--
 relativeMouse : (Int, Int) -> (Int, Int) -> (Int, Int)
 relativeMouse (ox, oy) (x, y) = (x - ox, y - oy)
 
 center : (Int, Int) -> (Int, Int)
 center (w, h) = (w //2, h // 2)
 
-type Vec = (Float, Float)
-type Pill = {pos : Vec, vel : Vec, rad : Float, col : Color }
+type alias Vec = (Float, Float)
+type alias Pill = {pos : Vec, vel : Vec, rad : Float, col : Color }
+
+
+vecAdd : Vec -> Vec -> Vec
+vecAdd (ax, ay) (bx, by) = ( ax + bx, ay + by )
+
+vecMulS : Vec -> Time.Time -> Vec
+vecMulS (x, y) s = (x*s, y*s)
 
 
 defaultPill =
-  { pos = (0, 100)
-  , vel = (0, -1)
+  { pos = (0, 0)
+  , vel = (0, -60)
   , rad = 15
   , col = lightRed
   }
 
-stepPill : Pill -> Pill
-stepPill p =
-  { p | pos = vecAdd p.pos p.vel }
+stepPill : Time.Time -> Pill -> Pill
+stepPill t p =
+  { p | pos = vecAdd p.pos <| vecMulS p.vel t }
 
-
-render (w, h) =
+render : (Int, Int) -> Pill -> Element
+render (w, h) pill =
   let
     formPill {rad, col, pos} =
       circle rad
       |> filled col
       |> move pos
-    forms = [ formPill defaultPill]
+    forms = [ formPill pill ]
   in
     color lightGray <| container w h middle
       <| color white
       <| collage 400 400 forms
 
+sig = Signal.map Time.inSeconds (Time.fps 30)
 
-main = Signal.map render Window.dimensions
+main = Signal.map2 render Window.dimensions (Signal.foldp stepPill defaultPill sig)
+--}
